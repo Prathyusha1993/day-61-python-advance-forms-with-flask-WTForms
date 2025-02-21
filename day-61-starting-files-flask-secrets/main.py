257a1ae393ecba419818c40200a32fdaa8d953cc
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-02-21 12:27:02 -0800

    day-61 python challenge forms with WTforms and bootstrap-flask

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 '''
 Red underlines? Install the required packages first: 
@@ -23,6 +24,7 @@ class LoginForm(FlaskForm):
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 app.secret_key = 'paibnhkiyram'
 
 
